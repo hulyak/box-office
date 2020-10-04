730592bb198441b55451d52a99e1dcf018e6d137
hulyak <57311842+hulya95@users.noreply.github.com>
2020-10-03 19:02:04 -0700

    added navbar and created page components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+import Nav from './components/Nav';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        {/* undefined route - default */}
+        <Route>
+          <div>This is 404 Page</div>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Starred.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Starred = () => {
+  return <div>this is start</div>;
+};
+
+export default Starred;
