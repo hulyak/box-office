2fb8636f1603d2544256e0d1cfa3f5a38a28e1b4
hulyak <57311842+hulya95@users.noreply.github.com>
2020-10-04 10:54:35 -0700

    create show page components and populate them with data

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../apis/config';
+import Cast from '../components/show/Cast';
+import Details from '../components/show/Details';
+import Seasons from '../components/show/Seasons';
+import ShowMainData from '../components/show/ShowMainData';
 
 const reducer = (prevState, action) => {
   switch (action.type) {
@@ -27,7 +31,7 @@ const Show = () => {
     reducer,
     initialState
   );
-  console.log(state);
+  // console.log(state);
 
   useEffect(() => {
     let isMounted = true;
@@ -50,10 +54,38 @@ const Show = () => {
 
   // console.log(show);
 
-  // if (isLoading) return <div>Loading</div>;
-  // if (error) return <div>Error occured: {error} </div>;
+  if (isLoading) return <div>Loading</div>;
+  if (error) return <div>Error occured: {error} </div>;
 
-  return <div>show page</div>;
+  // http://api.tvmaze.com/shows/1?embed[]=seasons&embed[]=cast
+  return (
+    <div>
+      <ShowMainData
+        image={show.image}
+        name={show.name}
+        rating={show.rating}
+        summary={show.summary}
+        tags={show.genres}
+      />
+
+      <div>
+        <h2>Details</h2>
+        <Details
+          status={show.status}
+          network={show.network}
+          premiered={show.premiered}
+        />
+      </div>
+      <div>
+        <h2>Seasons</h2>
+        <Seasons seasons={show._embedded.seasons} cast={show._embedded.cast} />
+      </div>
+      <div>
+        <h2>Cast</h2>
+        <Cast cast={show._embedded.cast} />
+      </div>
+    </div>
+  );
 };
 
 export default Show;
