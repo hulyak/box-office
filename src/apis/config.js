a16311de8b86c2fddfd8cb9ba7c6c320aeb8b2c6
hulyak <57311842+hulya95@users.noreply.github.com>
2020-10-03 20:19:53 -0700

    display show results and refactor fetch

---
+++ src/apis/config.js
@@ -0,0 +1,9 @@
+// encapsulate fetch logic
+const API_BASE_URL = 'http://api.tvmaze.com';
+
+export async function apiGet(queryString) {
+  const response = await fetch(`${API_BASE_URL}${queryString}`).then(response =>
+    response.json()
+  );
+  return response;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,34 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout.js';
+import { apiGet } from '../apis/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onSearch = () => {
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
-      .then(response => response.json())
-      .then(data => console.log(data));
+    apiGet(`/search/shows?q=${input}`).then(result => {
+      setResults(result);
+      // console.log(result);
+    });
   };
 
+  // conditionally render search result
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
+  };
   // make search when user presses enter key
   const onKeyDown = event => {
     if (event.keyCode === 13) {
@@ -28,6 +47,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </MainPageLayout>
   );
 };
