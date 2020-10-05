1a380d0481bc5572bff9792d2df291e043942eb7
hulyak <57311842+hulya95@users.noreply.github.com>
2020-10-04 22:34:02 -0700

    create a pwa

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 // hashRouter for client side routing only
 ReactDOM.render(
@@ -11,3 +12,8 @@ ReactDOM.render(
   </HashRouter>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
